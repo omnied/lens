0678364a0091fb19a2acec15f4c674d24521817a
Sebastian Malton <sebastian@malton.name>
2022-07-07 14:30:20 -0400

    Remove isTestEnv
    - Make all migration declarations injectable using a token
    
    - Make all migrations injectable using a token for the renderer side
      (instead of clearing them based on the truthiness of ipcRenderer)
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/getDiForUnitTesting.tsx
+++ src/renderer/getDiForUnitTesting.tsx
@@ -100,7 +100,6 @@ export const getDiForUnitTesting = (opts: { doGeneralOverrides?: boolean } = {})
     di.override(startTopbarStateSyncInjectable, () => ({
       run: () => {},
     }));
-
     di.override(terminalSpawningPoolInjectable, () => document.createElement("div"));
     di.override(hostedClusterIdInjectable, () => undefined);
 
